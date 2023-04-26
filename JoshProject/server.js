54ae8923ad87a8d3657ff5e1663e1a22ed185362
Mugema-Leo <lmugema@andrew.cmu.edu>
2023-04-26 17:20:45 +0200

    code

--- JoshProject/server.js
+++ JoshProject/server.js
@@ -26,8 +26,26 @@ app.get("/users", (req, res) => {
       });
   });
 
-  
+  app.post("/signup", (req, res) => {
+    const formData = req.body;
+    const jsonData = JSON.stringify(formData);
+    console.log(jsonData)
+    axios
+      .post("http://localhost:8080/Ehospital/MedicalServlet", jsonData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log("error");
+        console.log(error);
+      });
+  });
 
+  
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
